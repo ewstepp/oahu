d289df5248c5419b381676988e0ed6ea17c4f91e
Erik Stepp <erik.stepp@thoughtworks.com>
2012-10-16 00:45:36 -0400

    Initial ASP.NET MVC 4 template configured to allow OAuth logins

---
+++ Oahu/App_Start/AuthConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using Microsoft.Web.WebPages.OAuth;
+
+namespace Oahu
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            OAuthWebSecurity.RegisterMicrosoftClient(
+                clientId: ConfigurationManager.AppSettings["MicrosoftClientId"],
+                clientSecret: ConfigurationManager.AppSettings["MicrosoftClientSecret"]);
+
+            OAuthWebSecurity.RegisterTwitterClient(
+                consumerKey: ConfigurationManager.AppSettings["TwitterConsumerKey"],
+                consumerSecret: ConfigurationManager.AppSettings["TwitterConsumerSecret"]);
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: ConfigurationManager.AppSettings["FacebookAppId"],
+                appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"]);
+
+            OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
---
+++ Oahu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Oahu.MvcApplication" Language="C#" %>
