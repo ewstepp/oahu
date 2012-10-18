c2d1d2b8db85030e629bfdd2dc4dba6a9e35850f
Erik Stepp <erik.stepp@thoughtworks.com>
2012-10-17 23:00:59 -0400

    Adding unit test project.  Intentionally failing the build to test Azure Git deployment.

---
+++ Oahu.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Oahu.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void WillFail()
+        {
+            Assert.That(false, Is.True);
+        }
+    }
+}
